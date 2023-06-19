8f0f7bf2f8d7683abc1b74d70ec81af4a1506a36
damianTrybuszek <damian.trybuszek@gmail.com>
2023-06-19 20:15:59 +0200

    Sample Application that provide CRUD operation initialised

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from typing import List
 from uuid import uuid4
-
 from fastapi import FastAPI
-
+from starlette.middleware.cors import CORSMiddleware
 from models import User, Gender, Role
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 db: List[User] = [
     User(id=uuid4(), first_name="Katy", last_name="Perry", gender=Gender.female, roles=[Role.student]),
     User(id=uuid4(), first_name="Zakk", last_name="Wylde", gender=Gender.male, roles=[Role.admin, Role.user])
 ]
 
-
 @app.get("/")
 async def root():
     return {"Hello": "Damian"}
 
-
 @app.get("/api/v1/users")
 async def fetch_users():
     return db
-
 
 @app.post("/api/v1/users")
 async def register_user(user: User):
